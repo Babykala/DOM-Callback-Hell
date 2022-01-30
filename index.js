084ca23d0b323a6b9d07ff38aff4bfe75551acc5
Babykala <92169527+Babykala@users.noreply.github.com>
2022-01-30 07:47:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+setTimeout(()=>{
+    document.querySelector("#timer").innerText=10;
+    setTimeout(()=>{
+        document.querySelector("#timer").innerText=9;
+        setTimeout(()=>{
+            document.querySelector("#timer").innerText=8;
+            setTimeout(()=>{
+                document.querySelector("#timer").innerText=7;
+                setTimeout(()=>{
+                    document.querySelector("#timer").innerText=6;
+                    setTimeout(()=>{
+                        document.querySelector("#timer").innerText=5;
+                        setTimeout(()=>{
+                            document.querySelector("#timer").innerText=4;
+                            setTimeout(()=>{
+                                document.querySelector("#timer").innerText=3;
+                                setTimeout(()=>{
+                                    document.querySelector("#timer").innerText=2;
+                                    setTimeout(()=>{
+                                        document.querySelector("#timer").innerText=1;
+                                        setTimeout(()=>{
+                                            document.querySelector("#timer").innerText="Happy Independence Day!!";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+
+
+
